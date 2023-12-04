54b7e4ec69bc49e681848f866083dd2eb6b1a1e4
loew-tech <loew.technology@gmail.com>
2023-12-03 20:39:05 -0700

    day_3A

--- solutions.py
+++ solutions.py
@@ -1,6 +1,7 @@
 import re
 from functools import reduce
 from operator import mul
+from string import digits
 
 from utils import *
 from constants import *
@@ -41,8 +42,34 @@ def _day_2B(games):
     return sum(reduce(mul, marbles.values(), 1) for marbles in games.values())
 
 
+def day_3(part='A') -> int:
+    data = read_input(day=3)
+    ignore = set(f'{digits}.')
+    directions = tuple((i, j) for i in range(-1, 2)
+                       for j in range(-1, 2) if not i == j == 0)
+    sum_ = 0
+    for y, line in enumerate(data):
+        x, val, to_add = -1, '', False
+        while (x := x + 1) < len(line):
+            if line[x] in digits:
+                val += line[x]
+                for yi, xi in directions:
+                    to_add = to_add or 0 <= y + yi < len(
+                        data) and 0 <= x + xi < len(line) and \
+                             data[y + yi][x + xi] not in ignore
+
+            elif val and to_add:
+                sum_ += to_add * int(val)
+                val, to_add = '', False
+            else:
+                val = ''
+    return sum_
+
+
 if __name__ == '__main__':
     print(f'{day_1()=}')
     print(f'{day_1(part="B")=}')
     print(f'{day_2()=}')
     print(f'{day_2(part="B")=}')
+    print(f'{day_3()=}')
+    print(f'{day_3(part="B")=}')
